d0e6b76a37a9be610a1ee7131727cfa599401825
SecretAgent <iyengar.dhruv@outlook.com>
2023-10-06 20:00:41 -0700

    fixed it

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000; // Set the port for your server
